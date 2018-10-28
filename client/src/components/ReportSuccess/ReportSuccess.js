6d0d0bc4d1f31c8e75d7f320ec3004c4f8889a8c
Cody Rathor <codyrathor@gmail.com>
2018-10-27 17:44:36 -0700

    refactored report success to work for both safe site and best site pages

--- client/src/components/ReportSuccess/ReportSuccess.js
+++ client/src/components/ReportSuccess/ReportSuccess.js
@@ -1,60 +1,48 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import styles from './styles';
-import { UserContext } from '../../context/UserProvider';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { DASHBOARD } from '../../routes/routes';
 
 const ReportSuccess = ({
   classes,
-  safesite,
   leftBtnClick,
   leftBtnTitle,
   history,
   message,
-}) => {
-  return (
-    <UserContext.Consumer>
-      {({ logout }) => {
-        return (
-          <Grid container>
-            <Grid item xs={12}>
-              <p className={classes.reportMessage}>{message}</p>
-            </Grid>
-            <Grid item xs={12}>
-              <div className={classes.reportButtons}>
-                <Button
-                  variant="extendedFab"
-                  className={classes.reportButton}
-                  color="primary"
-                  onClick={leftBtnClick}
-                >
-                  {leftBtnTitle}
-                </Button>
-                <Button
-                  variant="extendedFab"
-                  className={classes.reportButton}
-                  color="secondary"
-                  onClick={() => history.push(DASHBOARD)}
-                >
-                  Back To DashBoard
-                </Button>
-              </div>
-            </Grid>
-          </Grid>
-        );
-      }}
-    </UserContext.Consumer>
-  );
-};
+}) => (
+  <Grid container>
+    <Grid item xs={12}>
+      <p className={classes.reportMessage}>{message}</p>
+    </Grid>
+    <Grid item xs={12}>
+      <div className={classes.reportButtons}>
+        <Button
+          variant="extendedFab"
+          className={classes.reportButton}
+          color="primary"
+          onClick={leftBtnClick}
+        >
+          {leftBtnTitle}
+        </Button>
+        <Button
+          variant="extendedFab"
+          className={classes.reportButton}
+          color="secondary"
+          onClick={() => history.push(DASHBOARD)}
+        >
+          Back To DashBoard
+        </Button>
+      </div>
+    </Grid>
+  </Grid>
+);
 
 ReportSuccess.propTypes = {
   classes: PropTypes.object.isRequired,
-  safesite: PropTypes.bool,
   leftBtnTitle: PropTypes.string,
   leftBtnClick: PropTypes.func,
   history: PropTypes.object.isRequired,
