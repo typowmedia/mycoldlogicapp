5fc4a4f3b05707b8f69330167daab1a31ef9ec83
Cody Rathor <codyrathor@gmail.com>
2018-10-29 21:18:21 -0700

    add functionality for coldlogic logo to show on all screens except dashboard on mobile

--- client/src/components/Navigation/NavigationBar/NavigationBar.js
+++ client/src/components/Navigation/NavigationBar/NavigationBar.js
@@ -1,18 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import {
-  withStyles,
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Hidden,
-} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ColdLogic from '../../../assets/ColdLogicLogo';
 import { withRouter } from 'react-router-dom';
 import DrawerNav from '../DrawerNav';
 import styles from './styles';
+import { DASHBOARD } from '../../../routes/routes';
+import ScreenSize from '../../../hoc/ScreenSize';
 
 class NavigationBar extends Component {
   constructor(props) {
@@ -22,50 +21,58 @@ class NavigationBar extends Component {
       showDrawer: false,
     };
   }
-  toggleDrawer = open => {
+  _toggleDrawer = open => {
     this.setState({ showDrawer: open });
   };
-  render() {
-    const { classes, user, history, logout } = this.props;
+  _showLogo = (location, currWidth, mobileScreenWidth) => {
+    if (location === DASHBOARD && currWidth <= mobileScreenWidth) return null;
     return (
-      <Fragment>
-        <AppBar position="fixed">
-          <Toolbar className={classes.toolbar}>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-              onClick={() => this.toggleDrawer(true)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="display1"
-              color="inherit"
-              className={classes.userWelcome}
-            >
-              Welcome {user.FirstName}
-            </Typography>
-            <Hidden xsDown>
-              <div
-                className={classes.logo}
-                onClick={() => {
-                  history.push('/dashboard');
-                }}
-              >
-                <ColdLogic color="#f4f4f4" />
-              </div>
-            </Hidden>
-          </Toolbar>
-        </AppBar>
-        <DrawerNav
-          show={this.state.showDrawer}
-          clicked={show => this.toggleDrawer(show)}
-          history={history}
-          logout={logout}
-          user={{ firstname: user.FirstName, lastname: user.LastName }}
-        />
-      </Fragment>
+      <div
+        className={this.props.classes.logo}
+        onClick={() => {
+          this.props.history.push(DASHBOARD);
+        }}
+      >
+        <ColdLogic color="#f4f4f4" />
+      </div>
+    );
+  };
+  render() {
+    const { classes, user, history, logout, location } = this.props;
+    return (
+      <ScreenSize>
+        {({ screenWidth, mobileScreen }) => (
+          <Fragment>
+            <AppBar position="fixed">
+              <Toolbar className={classes.toolbar}>
+                <IconButton
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="Menu"
+                  onClick={() => this._toggleDrawer(true)}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography
+                  variant="display1"
+                  color="inherit"
+                  className={classes.userWelcome}
+                >
+                  Welcome {user.FirstName}
+                </Typography>
+                {this._showLogo(location.pathname, screenWidth, mobileScreen)}
+              </Toolbar>
+            </AppBar>
+            <DrawerNav
+              show={this.state.showDrawer}
+              clicked={show => this._toggleDrawer(show)}
+              history={history}
+              logout={logout}
+              user={{ firstname: user.FirstName, lastname: user.LastName }}
+            />
+          </Fragment>
+        )}
+      </ScreenSize>
     );
   }
 }
@@ -73,7 +80,9 @@ class NavigationBar extends Component {
 NavigationBar.propTypes = {
   classes: PropTypes.object.isRequired,
   user: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
   logout: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
 export default withRouter(withStyles(styles)(NavigationBar));
