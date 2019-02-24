23d338f6f380608f69ec416552f3e295b0d6a396
Cody Rathor <codyrathor@gmail.com>
2019-02-24 15:24:27 -0800

    [FIX] make loading screen scale with screen

--- client/src/components/UI/LoadingScreen/styles.js
+++ client/src/components/UI/LoadingScreen/styles.js
@@ -1,7 +1,12 @@
 const styles = theme => ({
   loadingContainer: {
     height: '100vh',
+    maxWidth: theme.breakpoints.values.lg,
+    margin: '0 auto',
     padding: theme.spacing.unit * 10,
+    [theme.breakpoints.down('sm')]: {
+      padding: theme.spacing.unit / 2,
+    },
   },
   loadingItem: {
     padding: theme.spacing.unit * 3,
